3c3b2896e339c9b6a97ffdb727585c89f0a7139d
Andrew Morton <ahjmorton@gmail.com>
2018-02-05 22:09:03 +0000

    Standardise how commands are stood in

--- docker_standins.sh
+++ docker_standins.sh
@@ -4,28 +4,19 @@ docker_cmd() {
     echo 'docker run --rm --interactive --user "$(id -u):$(id -g)" --volume "$(pwd)":$(pwd) -w "$(pwd)" --tty' $@
 }
 
-if ! command -v ghci > /dev/null 
-then
-    alias ghci=$(docker_cmd haskell ghci)
-fi
+add_standin() {
+    local command_name=$1
+    shift
+    local args=$@
+    if ! command -v $command_name > /dev/null
+    then
+        echo "alias $command_name='$(docker_cmd $args)'"
+    fi
+}
 
-if ! command -v erl > /dev/null 
-then
-    alias erl=$(docker_cmd erlang erl)
-fi
-
-if ! command -v irb > /dev/null 
-then
-    alias irb=$(docker_cmd ruby)
-fi
-
-
-if ! command -v dot > /dev/null 
-then
-    alias dot=$(docker_cmd fgrehm/graphviz dot)
-fi
-
-if ! command -v curl > /dev/null 
-then
-    alias curl=$(docker_cmd byrnedo/apline-curl)
-fi
+eval $(add_standin ghci haskell ghci)
+eval $(add_standin erl erlang erl)
+eval $(add_standin irb ruby)
+eval $(add_standin dot fgrehm/graphviz dot)
+eval $(add_standin curl byrnedo/apline-curl)
+eval $(add_standin groovy groovy)
