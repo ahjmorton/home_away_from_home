2d93b2c546d6355aefff7ba69788dd9a6e6b1919
Andrew Morton <ahjmorton@gmail.com>
2018-02-05 21:26:53 +0000

    Added erlang

--- docker_standins.sh
+++ docker_standins.sh
@@ -5,6 +5,11 @@ then
     alias ghci='docker run --rm --interactive --volume "$(pwd)":/work -w /work --tty haskell ghci'
 fi
 
+if ! command -v erl > /dev/null 
+then
+    alias erl='docker run --rm --interactive --volume "$(pwd)":/work -w /work --tty erlang erl'
+fi
+
 if ! command -v dot > /dev/null 
 then
     alias dot='docker run --rm --interactive --volume "$(pwd)":/work -w /work fgrehm/graphviz dot'
