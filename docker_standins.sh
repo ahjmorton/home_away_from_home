cf33965be99525a2deeb3805f2c0dafc7be75c80
Andrew Morton <ahjmorton@gmail.com>
2018-02-05 21:16:34 +0000

    Add dotviz

--- docker_standins.sh
+++ docker_standins.sh
@@ -4,3 +4,8 @@ if ! command -v ghci > /dev/null
 then
     alias ghci='docker run --rm --interactive --volume "$(pwd)":/work -w /work --tty haskell ghci'
 fi
+
+if ! command -v dot > /dev/null 
+then
+    alias dot='docker run --rm --interactive --volume "$(pwd)":/work -w /work fgrehm/graphviz dot'
+fi
